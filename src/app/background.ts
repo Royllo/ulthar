f21b41e7f4e64af1303593abbffeb633b6081de3
oliverandrews <jaysonphillipmitchell@gmail.com>
2021-02-16 00:33:31 -0500

    Added placeholder basic auth system

--- src/app/background.ts
+++ src/app/background.ts
@@ -1,11 +1,12 @@
 chrome.runtime.onMessage.addListener(async(message, sender, sendResponse) => {
     console.log(`${message}`)
-    return chrome.storage.sync.get('url', async(data) =>{
+    return chrome.storage.sync.get('url', async(data) => {
       console.log(`${data.url}/link`);
       await fetch(`${data.url}/link`, {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
+          'Authorization': `Basic ${btoa(`user:hunter2`)}`
         },
         body: JSON.stringify({ url: message }),
       });
