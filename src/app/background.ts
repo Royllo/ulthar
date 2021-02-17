0d0aa9735e7a50020c0159fe45a4068040ddfcfe
oliverandrews <jaysonphillipmitchell@gmail.com>
2021-02-16 21:07:30 -0500

    Configured more clear code

--- src/app/background.ts
+++ src/app/background.ts
@@ -1,9 +1,9 @@
 chrome.runtime.onMessage.addListener(async(message, sender, sendResponse) => {
     console.log(`${message}`)
-    return chrome.storage.sync.get('url', async(data) => {
+    return chrome.storage.sync.get('url', async(urlData) => {
       return chrome.storage.sync.get('password', async(passwordData) => {
-        console.log(`${data.url}/link with password ${passwordData.password}`);
-        await fetch(`${data.url}/link`, {
+        console.log(`${urlData.url}/link with password ${passwordData.password}`);
+        await fetch(`${urlData.url}/link`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
